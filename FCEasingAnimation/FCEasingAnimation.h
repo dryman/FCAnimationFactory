4c404c8a8e1e4f0f5f9480a112acdd0e800b7393
James Montgomerie <jamie@th.ingsmadeoutofotherthin.gs>
2013-02-09 17:40:59 +0000

    Change the SDK to Xcode default - allows compilation with Xcode 4.6. Copy the headers when building the library so that they can be used in superprojects including this project as a submodule. Add new "FCEasingAnimation.h" header that includes other headers, to allow "#import <FCEasingAnimation/FCEasingAnimation.h>" to work when using the library in other projects.

---
+++ FCEasingAnimation/FCEasingAnimation.h
@@ -0,0 +1,11 @@
+//
+//  Header.h
+//  FCEasingAnimation
+//
+//  Created by James Montgomerie on 09/02/2013.
+//  Copyright (c) 2013 dryman. All rights reserved.
+//
+
+#import "FCAnimationFactory.h"
+#import "FCBasicAnimationFactory.h"
+#import "FCValueAnimationFactory.h"
